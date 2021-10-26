bc6e7691387f52e838b268d6d0cd83fd64315f28
lenaabdu <lenaabdu23@gmail.com>
2021-10-26 16:10:01 +0100

    make it better

--- src/index.js
+++ src/index.js
@@ -32,6 +32,13 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://lena:lena@cluster0.rqup
 // defining the Express app
 const app = express();
 const port = process.env.PORT || 7000
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -45,13 +52,6 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 app.post('/register', async(req, res) => {
     const newUser= req.body;
