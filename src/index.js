b9bbc9a6c7537817590421019504cb084d8cec58
lenaabdu <lenaabdu23@gmail.com>
2021-10-26 16:38:40 +0100

    make it better

--- src/index.js
+++ src/index.js
@@ -38,6 +38,13 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.post('/register', async(req, res) => {
     const newUser= req.body;
